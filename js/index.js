f271643b49bb32910683786c4c1db06227f78af1
Francisco Ochoa <fran8a90@gmail.com>
2022-08-30 18:59:41 -0300

    prueba 1

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let nombreCompleto = prompt("Ingresa tu nombre y apellido, por favor:")
+let calculadora = confirm ("Buenos días vamos a ver que bonificaciones tenemos disponibles según tu plan "  +  nombreCompleto)
+let plan = prompt("¿Cual es el código de tu plan aci25; aci26; aci27?")
+
+if (plan === "aci25") {
+    confirm("Tenes disponible un 60% de bonificación, ¿lo queres?")
+
+}
+if (plan === "aci26") {
+        confirm("Tenes disponible un 70% de bonificación, ¿lo queres?")
+}
+if (plan === "aci27") {
+    confirm("Tenes disponible un 80% de bonificación, ¿lo queres?")
+}   
+
+function calcular(plan, boni1) {
+    confirm("Resultado", plan * boni1)
+}
+
+
+
+
+
+
